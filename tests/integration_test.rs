2b3f1076d9fe82498cc63e1a0c6bc971a2a3adbf
DD5HT <hendrik@dd5ht.de>
2018-06-26 13:19:33 +0200

    added comments and a new implementation, a new test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,15 @@
 extern crate dxtracker;
 
 #[test]
-fn cluster_check() {
+fn cluster_insertion_test() {
     //TODO WIPE DATA before first insert
     let call = "TESTCALL";
     assert_eq!(dxtracker::insert_call(call), Some(call));
 
+}
+#[test]
+fn cluster_connection_test(){
+    let call = "DD5HT";
+    let cluster = "cluster.dl9gtb.de:8000";
+    assert_eq!(dxtracker::connect_to_cluster(cluster, call).is_ok(), true);
 }
