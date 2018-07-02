545034f548ba87c11a06de7255d13e36e16c7a14
DD5HT <hendrik@dd5ht.de>
2018-07-02 12:07:42 +0200

    removed uneeded things

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
 #![feature(vec_remove_item)]
 
-#[macro_use]
-extern crate lazy_static;
-
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::{OpenOptions, DirBuilder};
 use std::env;
 use std::io::ErrorKind::AlreadyExists;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 pub mod cluster;
 
